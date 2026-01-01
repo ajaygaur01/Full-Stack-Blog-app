25193a9b226fb7b7d8c3fe16ceaf4653086e94a0
AjayGaur <gaurajay787@gmail.com>
2026-01-02 00:21:21 +0530

    chnaged health check according to ingress

--- Backend/index.js
+++ Backend/index.js
@@ -31,9 +31,10 @@ app.use('/dashboard',DashboardRoutes)
 app.use('/comment',CommentRoutes)
 app.use('/public',PublicRoutes)
 
-app.get('/health', (req, res) => {
-    res.status(200).send('OK');
+app.get('/api/health', (req, res) => {
+    res.send('OK');
   });
+  
   
 
 app.listen(PORT,()=>{
